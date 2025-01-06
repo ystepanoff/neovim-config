72514566c6e490e18698f86185205db7eded3045
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-06 04:50:52 +0000

    Add new line on save

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,27 +1,37 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
+    theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    -- hl_override = {
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+    -- },
 }
 
 M.options = {
-    user = function ()
-        vim.opt.tabstop = 4
-        vim.opt.shiftwidth = 4
-        vim.opt.softtabstop = 4
-    end,
+    options = {
+        fixeol = true,
+    },
 }
 
 M.plugins = {}
+
+M.autocommands = function()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function()
+            local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            if #lines == 0 or lines[#lines] ~= "" then
+                vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
+            end
+        end,
+    })
+end
 
 return M
